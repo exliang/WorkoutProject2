da24c87f89fb95ed15d30fe0a92383b1416690c8
exliang <156244062+exliang@users.noreply.github.com>
2024-02-12 23:50:36 -0800

    Invalid key exception

--- key.py
+++ key.py
@@ -4,10 +4,14 @@
 # 79453973
 
 # key validation using private funcs
+class InvalidKeyException(Exception):
+    pass
+
+
 class Key:
-	def __key(self, key):  # return True if key is valid, else return false
-		try:
-			if type(int(key)) == int and int(key) > 0:
-				return True
-		except ValueError:
-			return False
+    def __key(self, key):  # return True if key is valid, else return false
+        try:
+            if type(int(key)) == int and int(key) > 0:
+                return True
+        except ValueError:
+            raise InvalidKeyException("Invalid key.")
