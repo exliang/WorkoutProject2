97dd186bb773e3819dcb1b6197322c099aa5bb8f
exliang <156244062+exliang@users.noreply.github.com>
2024-02-12 17:57:34 -0800

    validated key check & handled exceptions

--- wp2.py
+++ wp2.py
@@ -10,19 +10,25 @@ import key
 
 def main():
 
-	option = sys.argv[1]  # either -e or -d
-	file_input = sys.argv[2]
+    option = sys.argv[1]  # either -e or -d
+    file_input = sys.argv[2]
     file_output = sys.argv[3]
-    key = sys.argv[4]
+    key_value = sys.argv[4]
 
-    key.Key()  # validate the key first
-    if option == '-e':
-    	cryptography.encryption(file_input, file_output, key)
-    elif option == '-d':
-    	cryptography.decryption(file_input, file_output, key)
-    else:
-    	print("Invalid option: must type -e or -d. Try again.")
+    # validate the key first
+    if key.Key()._Key__key(key_value):  # calling the private key method in key.py
+        if option == '-e':
+            cryptography.encryption(file_input, file_output, int(key_value))
+        elif option == '-d':
+            cryptography.decryption(file_input, file_output, int(key_value))
+        else:
+            print("Invalid option: must type -e or -d. Try again.")
+    else:  # invalid key
+    	print("Invalid key: must be an integer. Try again.")
 
 
 if __name__ == '__main__':
 	main()
+
+# Citations:
+# - https://stackoverflow.com/questions/40852686/calling-a-private-class-member-function-in-python-from-another-module
