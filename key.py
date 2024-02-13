86cf0f36b9ed15e9bad816e4eb72e375dfffdc28
exliang <156244062+exliang@users.noreply.github.com>
2024-02-12 17:56:50 -0800

    created private func to validate key

--- key.py
+++ key.py
@@ -2,3 +2,12 @@
 # Emily Liang
 # exliang@uci.edu
 # 79453973
+
+# key validation using private funcs
+class Key:
+	def __key(self, key):  # return True if key is valid, else return false
+		try:
+			if type(int(key)) == int and int(key) > 0:
+				return True
+		except ValueError:
+			return False
