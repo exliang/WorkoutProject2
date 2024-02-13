9b2b705480fea377a307965fb2bcb0f1da96a11e
exliang <156244062+exliang@users.noreply.github.com>
2024-02-12 23:51:05 -0800

    decryption func

--- cryptography.py
+++ cryptography.py
@@ -13,7 +13,8 @@ def encryption(file_input, file_output, key_value):
     matrix = []
     row = []
     for i in range(len(text)):
-        if i + 1 == key_value or ((i + 1) % key_value == 0 and key_value < i):  # i = key_value or a multiple of key_value
+        # i = key_value or a multiple of key_value
+        if i + 1 == key_value or ((i + 1) % key_value == 0 and key_value < i):
             row.append(text[i])
             matrix.append(row)
             row = []
@@ -22,14 +23,14 @@ def encryption(file_input, file_output, key_value):
             matrix.append(row)
         else:  # i is any num in besides key_value or a multiple of key_value
             row.append(text[i])
-    if len(matrix[len(matrix)-2]) != len(matrix[len(matrix)-1]):  # last row in matrix has empty spots
-    	while len(matrix[len(matrix)-1]) < len(matrix[len(matrix)-2]):
-    		matrix[len(matrix)-1].append("#")  # add a hashtag in place of the empty spots
-    # printing_matrix(matrix) #DELETE AFTER TESTING
+    # last row in matrix has empty spots
+    if len(matrix[len(matrix)-2]) != len(matrix[len(matrix)-1]):
+        while len(matrix[len(matrix)-1]) < len(matrix[len(matrix)-2]):
+            # add a hashtag in place of the empty spots
+            matrix[len(matrix)-1].append("#")
 
     # transpose the matrix using key
     transposed_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-    # printing_matrix(transposed_matrix) #DELETE AFTER TESTING
 
     # put text back into string format
     encrypted_text = ""
@@ -44,9 +45,43 @@ def encryption(file_input, file_output, key_value):
 
 
 def decryption(file_input, file_output, key_value):
-	pass
+    f = open(file_input, "r")  # open original text file to read
+    text = f.read()  # read the text
 
+    num_cols = math.ceil(len(text) / key_value)
+    matrix_spots = key_value * num_cols  # key_value = num rows
+    emtpy_positions = matrix_spots - len(text)  # pos that shouldnt be filled
 
-def printing_matrix(matrix): #DELETE AFTER PROGRAM WORKS
-	for row in matrix:
-		print(row)
+    # intialize matrix w/correct cols & rows
+    matrix = [[0 for _ in range(num_cols)] for _ in range(key_value)]
+    bottom_col = num_cols - 1
+    bottom_row = key_value - 1
+
+    # go up empty positon times, decrease row
+    while emtpy_positions > 0:
+        matrix[bottom_row][bottom_col] = "~~"
+        emtpy_positions -= 1
+        bottom_row -= 1
+
+    i = 0
+    for row in range(key_value):
+        for col in range(num_cols):
+            if matrix[row][col] == "~~":
+                continue
+            else:
+                matrix[row][col] = text[i]
+                i += 1
+
+    # transpose the matrix using key
+    transposed_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+    # put text back into string format
+    decrypted_text = ""
+    for row in transposed_matrix:
+        for col in row:
+            if col != "~~":
+                decrypted_text += col
+
+    # write the decrypted text to decryptedtext.txt
+    f = open(file_output, "w")
+    f.write(decrypted_text)
