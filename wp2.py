1293fb98a0a8049c6766cb3abbad964168b8e4ec
exliang <156244062+exliang@users.noreply.github.com>
2024-02-12 16:22:29 -0800

    Update wp2.py

--- wp2.py
+++ wp2.py
@@ -3,3 +3,26 @@
 # exliang@uci.edu
 # 79453973
 
+import sys
+import cryptography 
+import key
+
+
+def main():
+
+	option = sys.argv[1]  # either -e or -d
+	file_input = sys.argv[2]
+    file_output = sys.argv[3]
+    key = sys.argv[4]
+
+    key.Key()  # validate the key first
+    if option == '-e':
+    	cryptography.encryption(file_input, file_output, key)
+    elif option == '-d':
+    	cryptography.decryption(file_input, file_output, key)
+    else:
+    	print("Invalid option: must type -e or -d. Try again.")
+
+
+if __name__ == '__main__':
+	main()
